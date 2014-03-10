af4170b3c323511cfe22beec867f8b728659a1ee
Ladochu <lachu20012002@gmail.com>
2014-03-10 09:40:19 -0400

    move statements into module CHT::Config

--- config.rb
+++ config.rb
@@ -1,9 +1,13 @@
-#! /usr/bin/env ruby
+module CHT
 
-dispatcher={:address => '192.168.10.112', :port  => 10000}
-worker={
-    'cht2' => {:address => '192.168.10.243',    :port => 10001},
-    'cht3' => {:address => '192.168.10.133',    :port => 10002},
-    'cht4' => {:address => '192.168.10.229',    :port => 10003},
-    'cht5' => {:address => '192.168.10.18',     :port => 10004}
-}
+  module Config
+    DISPATCHER={:address => '192.168.10.112', :port  => 10000}
+    WORKER={
+      'cht2' => {:address => '192.168.10.243',    :port => 10001},
+      'cht3' => {:address => '192.168.10.133',    :port => 10002},
+      'cht4' => {:address => '192.168.10.229',    :port => 10003},
+      'cht5' => {:address => '192.168.10.18',     :port => 10004}
+    }
+  end
+
+end
