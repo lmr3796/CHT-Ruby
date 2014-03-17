cd019f526b7e5e3b78a2668f003e16c0cbb3cf36
lmr3796 <lmr3796joseph@gmail.com>
2014-03-17 18:44:15 +0800

    Change all "state" usage to "STATUS" to assure consistency

--- src/status_checker.rb
+++ src/status_checker.rb
@@ -7,19 +7,19 @@ class StatusChecker
   def initialize(worker_table={})
     # TODO: make up a worker table
     @worker_table = worker_table.clone
-    @worker_status_table = Hash[worker_table.map{|w_id, w| [w_id, Worker::STATE::UNKNOWN]}]
+    @worker_status_table = Hash[worker_table.map{|w_id, w| [w_id, Worker::STATUS::UNKNOWN]}]
   end
   def release_worker(worker)
-    @worker_status_table[worker] = Worker::STATE::AVAILABLE
+    @worker_status_table[worker] = Worker::STATUS::AVAILABLE
   end
   def occupy_worker(worker)
-    @worker_status_table[worker] = Worker::STATE::OCCUPIED
+    @worker_status_table[worker] = Worker::STATUS::OCCUPIED
   end
   def worker_running(worker)
-    @worker_status_table[worker] = Worker::STATE::BUSY
+    @worker_status_table[worker] = Worker::STATUS::BUSY
   end
-  def collect_state(worker=nil)
-    # TODO: collect state of all/specified worker
+  def collect_status(worker=nil)
+    # TODO: collect status of all/specified worker
     raise NotImplementedError
   end
   # TODO: worker registration at runtime?
