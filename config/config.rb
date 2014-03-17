fe48a9b63ed2666f947ca8767f575c8ad1c5b032
lmr3796 <lmr3796joseph@gmail.com>
2014-03-17 21:00:37 +0800

    Same port for all workers

--- config/config.rb
+++ config/config.rb
@@ -3,9 +3,9 @@ module CHT_Configuration
     DISPATCHER={:address => '192.168.10.112', :port  => 10000}
     WORKER={
       'cht2' => {:address => '192.168.10.243',    :port => 10001},
-      'cht3' => {:address => '192.168.10.133',    :port => 10002},
-      'cht4' => {:address => '192.168.10.229',    :port => 10003},
-      'cht5' => {:address => '192.168.10.18',     :port => 10004}
+      'cht3' => {:address => '192.168.10.133',    :port => 10001},
+      'cht4' => {:address => '192.168.10.229',    :port => 10001},
+      'cht5' => {:address => '192.168.10.18',     :port => 10001}
     }
 
     def self.druby_uri(socket)
