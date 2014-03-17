739435ff8f882253d45705bd2d7471d3b1b87a96
Ladochu <lachu20012002@gmail.com>
2014-03-17 07:41:37 -0400

    implement deadline based scheduling algorithm

--- src/algorithms/abstract_algorithm.rb
+++ src/algorithms/abstract_algorithm.rb
@@ -1,11 +1,19 @@
 module SchedulingAlgorithm
+  USABLE_WORKER_STATUS = [
+    Worker::STATUS::OCCUPIED,
+    Worker::STATUS::AVAILABLE,
+    Worker::STATUS::BUSY
+  ]
   class AbstractAlgorithm
     def initialize()
       raise "Cannot directly instantiate an AbstractAlgorithm." if self.class == AbstractAlgorithm
     end
-    def schedule_job(job_list, worker_status)
+    def schedule_job(job_list, worker_status, current_schedule)
+      # job_list: {job_id => Job instance}
+      # worker_status: {worker_id => status}
+      # current_schedule: {job_id => [worker_id, ...]}
+      # Return: {job_id => [worker_id, ...]}
       raise NotImplementedError
-      #return {job_id => [worker1, worker2...]}
     end
   end
 end
