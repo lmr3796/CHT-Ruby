081db0ef12bdcf46caa46dbf13525df45888f5b2
lmr3796 <lmr3796joseph@gmail.com>
2014-03-19 02:44:06 +0800

    dispatcher.joblist is a JobList

--- src/dispatcher.rb
+++ src/dispatcher.rb
@@ -87,9 +87,9 @@ class Dispatcher
 
   def initialize(arg={})
     raise ArgumentError.new(arg.to_s) if !(arg.keys-[:status_checker, :decision_maker]).empty?
-    @job_list = ReadWriteLockHash.new
+    @job_list = JobList.new
     @job_list.subscribe_job_list_change(self)
-    @schedule_manager = ScheduleManager.new(job_list)
+    @schedule_manager = ScheduleManager.new(@job_list)
     @job_worker_queues = ReadWriteLockHash.new
     @resource_mutex = Mutex.new
     @status_checker = arg[:status_checker]
