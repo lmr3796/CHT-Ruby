dbad052311ef88611aaf33fe29d2080f651617c7
lmr3796 <lmr3796joseph@gmail.com>
2014-06-23 00:58:08 +0800

    Add skeleton of synthesizer

---
+++ experiment/thesis/workload_synthesizer.rb
@@ -0,0 +1,34 @@
+#! /usr/bin/env ruby
+
+require 'optparse'
+require 'time'
+
+require_relative 'workload_parser.rb'
+
+# Option parsing
+OptionParser.new do |opts|
+  $options={}
+  # This displays the help screen, all programs are
+  # assumed to have this option.
+  opts.on( '-h', '--help', 'Display this screen' ){|h| puts opts }
+  opts.on( '-r', '--rate r', Float, 'Set job sampling rate' ){|r| $options[:job_sample_rate] = r }
+  opts.on( '-f', '--file file_name', 'Set workload file name' ) do |f|
+    begin
+      file = open f
+      $options[:input] = file
+    rescue
+      puts "Can't open file #{f}"
+      exit(-1)
+    end
+  end
+end.parse!
+begin
+  f = ARGV.shift
+  $options[:input] ||= open(f) if f
+  $options[:input] ||= STDIN
+rescue
+  puts "Can't open file #{f}"
+  exit(-1)
+end
+
+jobs = StandardWorkloadFormatParser.from_file $options[:input]
