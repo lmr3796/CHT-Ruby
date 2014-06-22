6d9cd67009c960375f3bc826eef4a1c47370c376
lmr3796 <lmr3796joseph@gmail.com>
2014-06-22 23:21:17 +0800

    Add SWF parser

---
+++ experiment/thesis/workload_parser.rb
@@ -0,0 +1,30 @@
+require 'csv'
+module StandardWorkloadFormatParser
+  FIELDS = [
+    :job_number,
+    :submit_time,
+    :wait_time,
+    :run_time,
+    :allocated_processors,
+    :average_cpu_time,
+    :used_memory,
+    :requested_processors,
+    :requested_time,
+    :requested_memory,
+    :status,
+    :user_id,
+    :group_id,
+    :executable_number,
+    :queue_number,
+    :partition_number,
+    :preceding_job_number,
+    :think_time_from_preceding,
+  ]
+  def from_file(f)
+    return f.map{|l| from_line l.strip}
+  end
+  def from_line(l)
+    return Hash[FIELDS.zip(l.split.map{|s|s.to_i})]
+  end
+  module_function :from_line, :from_file
+end
