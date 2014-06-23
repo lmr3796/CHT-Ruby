2f447ba08d4c941f2fc4f3be85b6fbd132259ce6
lmr3796 <lmr3796joseph@gmail.com>
2014-06-23 14:21:23 +0800

    Add option to limit runtime

--- experiment/thesis/workload_synthesizer.rb
+++ experiment/thesis/workload_synthesizer.rb
@@ -11,8 +11,9 @@ OptionParser.new do |opts|
   # This displays the help screen, all programs are
   # assumed to have this option.
   opts.on( '-h', '--help', 'Display this screen' ){|h| puts opts }
-  opts.on( '-r', '--sample-rate r', Float, 'Set job sampling rate' ){|r| $options[:job_sample_rate] = r }
-  opts.on( '-s', '--scale-rate r', Float, 'Set job sampling rate' ){|r| $options[:job_scale_rate] = r }
+  opts.on( '-r', '--sample-rate rate', Float, 'Set job sampling rate' ){|r| $options[:job_sample_rate] = r }
+  opts.on( '-s', '--scale-rate rate', Float, 'Set job scale rate' ){|r| $options[:job_scale_rate] = r }
+  opts.on( '-t', '--run-time-limit limit', Integer, 'Set job runtime limit' ){|r| $options[:job_time_limit] = r }
   opts.on( '-f', '--file file_name', 'Set workload file name' ) do |f|
     begin
       file = open f
