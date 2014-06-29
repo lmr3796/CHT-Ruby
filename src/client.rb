383bfe543d8bc513f5f13285c74758d35c5820f5
lmr3796 <lmr3796joseph@gmail.com>
2014-06-30 01:53:55 +0800

    Refine some code

--- src/client.rb
+++ src/client.rb
@@ -79,80 +79,49 @@ class Client
     @logger.info "Unregistered from the system"
   end
 
+  def wait_all()
+    @thread_id_list.each{|thread_id| wait(thread_id)}
+  end
+
   def start(blocking=false)
-    # DEBUG!!!
     @uuid = @dispatcher.register_client
     @logger.info "Registered client to the system, uuid=#{@uuid}"
     @msg_service = MessageService.new(@uuid, @dispatcher, @logger)
     @msg_service.start
 
-    # TODO: send_jobs
-    return
-    # DEBUG!!!
-    job_id_list = send_jobs(@jobs)
-    # TODO a better way to send out result
-    @result = Array.new(job_id_list.size){Hash.new}
-    @thread_id_list = job_id_list.each_with_index.map{ |job_id,i|
-      @result[i][:deadline] = @jobs[i].deadline
-      @thread_pool.schedule{
-        run_job(job_id, i)
-      }
-    }
+    #TODO: register on worker available handler
+
+    #job_id_list = send_jobs(@jobs)
+    ## TODO a better way to send out result
+    #@result = Array.new(job_id_list.size){Hash.new}
+    #@thread_id_list = job_id_list.each_with_index.map{ |job_id,i|
+    #  @result[i][:deadline] = @jobs[i].deadline
+    #  @thread_pool.schedule{
+    #    run_job(job_id, i)
+    #  }
+    #}
     return @thread_id_list unless blocking
     wait_all
   end
 
-  def wait_all()
-    @thread_id_list.each{|thread_id| wait(thread_id)}
-  end
-
-  def wait(thread_id)
-    @thread_pool.join(thread_id)
-  end
-
-  def run_job(job_id, result_pos)
-    @result[result_pos][:deadline] = Time.now
-    task_queue = @submitted_jobs[job_id]
-    thread_id_list = []
-    until task_queue.empty? do
-      until task_queue.empty? do
-        task = task_queue.pop
-        @logger.debug "#{job_id} waiting for worker"
-        worker = @dispatcher.require_worker(job_id)
-        @logger.debug "#{job_id} assigned with worker #{worker}"
-        thread_id_list << @thread_pool.schedule {
-          #TODO: Task execution failure???
-          run_task_on_worker(task, job_id, worker)
-        }
-      end
-      thread_id_list.each{ |thread_id|
-        @thread_pool.join(thread_id)
-      }
-    end
-    @logger.info "Job #{job_id} is done"
-    @result[result_pos][:finish_time] = Time.now
-    @dispatcher.job_done(job_id)
-  end
-  private :run_job
-
   def send_jobs(jobs)
     # Convert to a job list if a single job passed
+    raise ArgumentError if jobs == nil
     jobs = [jobs] unless jobs.is_a? Array
-    jobs.each {|x| raise 'Parameters should be a list of jobs or a single job' if !x.is_a? Job}
+    jobs.select{!x.is_a? Job}.empty? or raise ArgumentError, 'Parameters should be a list of jobs or a single job'
+
     @logger.info "Submitting #{jobs.size} job(s)"
     job_id_list = @dispatcher.submit_jobs(jobs)
     (@logger.error "Submission failed"; raise 'Submission failed') if !job_id_list or !job_id_list.is_a? Array
     @logger.info "Job submitted: id mapping: #{job_id_list}"
     # Build a task queue for each job, indexed with job_id returned from dispatcher
-    job_id_list.each_with_index{|job_id, index|
-      task_queue = Queue.new
-      tasks = jobs[index].task
-      tasks.each {|x| task_queue.push x}
+    job_id_list.each_with_index do |job_id, i|
+      task_queue = Queue.new  # task_queue of a job must be synchronized for it's consumed under multithreaded env.
+      jobs[i].task.each {|t| task_queue << t}
       @submitted_jobs[job_id] = task_queue
-    }
+    end
     return job_id_list
   end
-  private :send_jobs
 
   def run_task_on_worker(task, job_id, worker)
     # TODO: Task execution failure???
