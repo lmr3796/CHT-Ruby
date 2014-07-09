d251cc006350af0a316f15f3b94a1bd4fb1891fc
lmr3796 <lmr3796joseph@gmail.com>
2014-07-09 20:09:40 +0800

    Spearate runner into generator and runner

--- experiment/thesis/run_wagap.sh
+++
@@ -1,2 +0,0 @@
-#! /bin/bash
-./workload_runner.rb -d 2 -b 1 -c 0.01 -w 0.1 -s 0.01 -f wagap-2013.swf $@
--- experiment/thesis/workload_runner.rb
+++ experiment/thesis/workload_runner.rb
@@ -20,75 +20,46 @@ OptionParser.new do |opts|
   # This displays the help screen, all programs are
   # assumed to have this option.
   opts.on( "-h", "--help", "Display this screen"){puts opts; exit}
-  opts.on("-s rate", "--sample-rate rate", Float, "Set job sampling rate. #{$options[:job_sample_rate]} by default") do |r|
-    $options[:job_sample_rate] = r
-  end
-  opts.on("-d rate", "--deadline-rate rate", Float, "Set deadline (ratio to runtime). #{$options[:deadline_rate]} by default") do |r|
-    $options[:deadline_rate] = r
-  end
-  opts.on("-c rate", "--cpu-scale-rate rate", Float, "Set job scale rate. #{$options[:job_scale_rate]} by default") do |r|
-    $options[:job_scale_rate] = r
-  end
-  opts.on("-w rate", "--wait-time-scale-rate rate", Float, "Set waiting time scale rate. #{$options[:wait_time_scale_rate]} by default") do |r|
-    $options[:wait_time_scale_rate] = r
-  end
-  opts.on("-b time", "--batching-threshold time", Float, "Set the threshold to batch jobs together. #{$options[:batch_threshold]} by default") do |r|
-    $options[:batch_threshold] = r
-  end
-  opts.on("--serialize file", String, "Serialize the batch and dump to target file instead of executing it.") do |r|
-    $options[:serialize] = r
-  end
-  opts.on("--deserialize", "The input file is a serialized batch."){$options[:deserialize]=true}
-  #opts.on( '-t lower,upper', '--run-time-limit lower,upper', Array, 'Set job run time limit' ) do |r|
-  #  $options[:job_exec_time_limit] = Range.new r[0].to_f, r[1].to_f
-  #end
-  #opts.on( '-T limit', '--wait-time-limit limit', Float, 'Set job wait time limit' ) do |r|
-  #  $options[:job_wait_time_limit] = r
-  #end
   opts.on('--dry-run', "Don't submit jobs, give job info"){$options[:dry_run]=true}
-  opts.on( '-f', '--file file_name', 'Set workload file name' ) do |f|
+
+  opts.on( '-i', '--input file_name', 'Set workload file name' ) do |f|
     begin
       file = open f
       $options[:input] = file
     rescue
-      puts "Can't open file #{f}"
+      puts "Can't open input file #{f}"
+      exit(-1)
+    end
+  end
+
+  opts.on( '-o', '--output file_name', 'Set output file name' ) do |f|
+    begin
+      file = open(f, 'w')
+      $options[:output] = file
+    rescue
+      puts "Can't open output file #{f}"
       exit(-1)
     end
   end
 end.parse!
 
-begin
-  f = ARGV.shift
-  $options[:input] ||= open(f) if f
-  $options[:input] ||= STDIN
-rescue
-  puts "Can't open file #{f}"
-  exit(-1)
-end
-
-$options[:logger] = Logger.new(STDERR)
-if $options[:deserialize]
-  $stderr.puts "Deserialize from file."
-  jobs, batch = Marshal.load($options[:input].read) 
-  runner = WorkloadSynthesizer.new jobs, $options
-else
-  $stderr.puts "Parse from swf file."
-  jobs = StandardWorkloadFormatParser.from_file $options[:input]
-  runner = WorkloadSynthesizer.new jobs, $options
-  batch = runner.gen_workload
-end
-if $options[:serialize]
-  dump = Marshal.dump([jobs, batch])
+if(f = ARGV.shift)
+  raise 'Extra argument specified' if $options[:input]
   begin
-    output_file = open($options[:serialize], "w")
+    $options[:input] = open(f)
   rescue
-    puts "Can't open file #{f}"
+    puts "Can't open input file #{f}"
     exit(-1)
   end
-  raise "Output failed." if output_file.write(dump) != dump.size
-  exit
 end
+$options[:input] ||= $stdin
+$options[:output] ||= $stdout
+
+$options[:logger] = Logger.new(STDERR)
+$stderr.puts "Deserialize from input."
+jobs, batch = Marshal.load($options[:input].read) 
+runner = WorkloadSynthesizer.new jobs, $options
 $stderr.puts "Total #{batch.size} batches, #{batch.map{|b| b[:batch].size}.reduce(:+)} jobs to simulate"
 exit if !!$options[:dry_run]
 jobs, finish_time = runner.run(batch)
-p "#{finish_time.select{|j, t| jobs[j][:deadline] >= t}.size} out of #{jobs.size} jobs met deadline."
+$options[:output].puts "#{finish_time.select{|j, t| jobs[j][:deadline] >= t}.size} out of #{jobs.size} jobs met deadline."
