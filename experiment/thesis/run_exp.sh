bbbecc59f44388e87b0b74697d26b9c751f88c7b
lmr3796 <lmr3796joseph@gmail.com>
2014-07-23 15:51:23 +0800

    Change log name

--- experiment/thesis/run_exp.sh
+++ experiment/thesis/run_exp.sh
@@ -2,6 +2,6 @@
 set -e
 for f in $@; do
 	echo "Running $f"
-	(time ./workload_runner.rb -i $f -o ${f}.out 2>client_log) >> ${f}.res 2>&1 
+	(time ./workload_runner.rb -i $f -o ${f}.out 2>client.log) >> ${f}.res 2>&1 
 done
 killall ruby
