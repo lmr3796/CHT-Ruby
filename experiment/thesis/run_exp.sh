2bf386068d92f739ba1fb26593f7255b97cd7277
lmr3796 <lmr3796joseph@gmail.com>
2014-07-25 23:53:53 +0800

    Run exp writes current running set to current.log

--- experiment/thesis/run_exp.sh
+++ experiment/thesis/run_exp.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 set -e
+echo '' > current.log
 for f in $@; do
-	echo "Running $f"
+	echo "Running $f" >> current.log
 	(time ./workload_executer.rb -i $f -o ${f}.out 2>client.log) >> ${f}.res 2>&1 
 done
