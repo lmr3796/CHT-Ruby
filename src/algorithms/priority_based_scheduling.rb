50ec362dcb3c45de1940b8c41f39910bb08dbc37
lmr3796 <lmr3796joseph@gmail.com>
2014-09-19 19:11:07 +0800

    Fix Priority based scheduling

--- src/algorithms/priority_based_scheduling.rb
+++ src/algorithms/priority_based_scheduling.rb
@@ -2,6 +2,9 @@ require_relative 'abstract_algorithm'
 
 module SchedulingAlgorithm
   class PriorityBasedScheduling < AbstractAlgorithm
+    PRESERVE_RATE=0.3
+    def initialize()
+    end
     def schedule_job(job_list, worker_status, arg={})
       # job_list: {job_id => Job instance}
       # worker_status: {worker_id => status}
@@ -12,24 +15,25 @@ module SchedulingAlgorithm
       job_id_by_priority = job_list.keys.sort_by{ |job_id| job_list[job_id].priority }.reverse
       remaining_worker = worker_status.keys
       schedule_result = {}
+
+      return schedule_result if job_list.empty?
+
+      # Assign as more worker as we can for job with high priority
+      job = job_list[job_id_by_priority[0]]
+      worker_by_throughput = remaining_worker.sort_by{ |worker_id| job.task_running_time_on_worker[worker_id] }
+      worker_needed = [(worker_by_throughput.size * (1 - PRESERVE_RATE)).round, job.progress.undone.size].min
+      schedule_result[job_id_by_priority[0]] = worker_by_throughput.slice!(0, worker_needed)
+      remaining_worker = worker_by_throughput
+
+
       # Assign a worker for each job
-      job_id_by_priority.each{ |job_id|
+      job_id_by_priority[1..-1].each do |job_id|
         break if remaining_worker.empty?
         job = job_list[job_id]
-        best_worker_index = (0...remaining_worker.size).min_by{ |index| job.task_running_time_on_worker[remaining_worker[index]] }
+        best_worker_index = (0..-1).min_by{|i| job.task_running_time_on_worker[remaining_worker[i]]}
         schedule_result[job_id] = [remaining_worker[best_worker_index]]
         remaining_worker.delete_at(best_worker_index)
-      }
-      # Assign as more worker as we can for job with high priority
-      job_id_by_priority.each{ |job_id|
-        break if remaining_worker.empty?
-        job = job_list[job_id]
-        worker_by_throughput = remaining_worker.sort_by{ |worker_id| job.task_running_time_on_worker[worker_id] }
-        worker_needed = [worker_by_throughput.size, job.progress.undone.size].min
-        schedule_result[job_id] << worker_by_throughput.slice!(0, worker_needed)
-        schedule_result[job_id].flatten!
-        remaining_worker = worker_by_throughput
-      }
+      end
       return schedule_result
     end
   end
