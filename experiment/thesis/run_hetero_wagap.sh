37be590432f0189f6be1c983aef286e14b7f8358
lmr3796 <lmr3796joseph@gmail.com>
2014-09-20 23:30:24 +0800

    Update scripts to run multiple algorithms

---
+++ experiment/thesis/run_hetero_wagap.sh
@@ -0,0 +1,17 @@
+D_MAKER_ADDR='druby://localhost:10003'
+
+echo 'Running hetero Priority'
+../../algorithm_changer.rb $D_MAKER_ADDR PriorityBasedScheduling 
+./run_exp.sh wagap-d5-b1-c0_001-w0_1-s0_01-hetero-Priority/wagap.sample.?
+
+echo 'Running hetero EDF'
+../../algorithm_changer.rb $D_MAKER_ADDR EarliestDeadlineFirstScheduling 
+./run_exp.sh wagap-d5-b1-c0_001-w0_1-s0_01-hetero-EarliestDeadlineFirst/wagap.sample.?
+
+echo 'Running hetero DeadlineNoPriority'
+../../algorithm_changer.rb $D_MAKER_ADDR PreemptiveNoPriorityDeadlineBasedScheduling 
+./run_exp.sh wagap-d5-b1-c0_001-w0_1-s0_01-hetero-DeadlineNoPriority/wagap.sample.?
+
+echo 'Running hetero DeadlinePriority'
+../../algorithm_changer.rb $D_MAKER_ADDR PreemptiveDeadlineBasedScheduling 
+./run_exp.sh wagap-d5-b1-c0_001-w0_1-s0_01-hetero-DeadlinePriority/wagap.sample.?
